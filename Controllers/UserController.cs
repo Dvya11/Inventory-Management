3fe718b53bb639855bf901800bb1c5ada4b809c0
dushyantsinhChudasama <dushyant9803@gmail.com>
2026-03-27 13:41:49 +0530

    Added Layout

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,15 +4,9 @@ namespace WebApplication1.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
-        {
-            return View("Dashboard");
-        }
-
         public IActionResult Dashboard()
         {
-            // render the existing Views/User/Index.cshtml
-            return View("Index");
+            return View("Dashboard");
         }
     }
 }
