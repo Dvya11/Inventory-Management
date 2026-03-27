663e1844aea47489fe740b68f22e9d08ae5f8226
Divya Chauhan <divyachauhn20061@gmail.com>
2026-03-27 13:17:56 +0530

    add the Dashboard

---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Dashboard");
+        }
+
+        public IActionResult Dashboard()
+        {
+            // render the existing Views/User/Index.cshtml
+            return View("Index");
+        }
+    }
+}
